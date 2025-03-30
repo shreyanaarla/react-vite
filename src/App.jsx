3e16b8e1629e060a39ce86635d38fa711f9ac863
shreyanaarla <131688075+shreyanaarla@users.noreply.github.com>
2025-03-30 23:19:10 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,77 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const books = [
+  {title: "A Court of Thorns and Roses", author: "Sarah J. Maas", genre: "Romance", isbn: "9781619634459", FilmAdaptation: false},
+  {title: "Caraval", author: "Stephanie Garber", genre: "Mystery", isbn: "9781250095268", FilmAdaptation: false},
+  {title: "Red Queen", author: "Victoria Aveyard", genre: "Dystopian", isbn: "9781250095268", FilmAdaptation: false},
+  {title: "City of Bones", author: "Cassandra Clare", genre: "Fantasy", isbn: "9781250095268", FilmAdaptation: true},
+  {title: "The Lightning Thief", author: "Rick Riordan", genre: "Mythology", isbn: "9781250095268", FilmAdaptation: true},
+];
+
+let book = {title: "", author: "", genre: ""}
+
+function ListBooks() {
+  return (
+    <>
+      <ul>
+        for (let b of books) {
+          <li>{b.title}</li>
+        }
+      </ul>
+    </>
+  );
+}
+
+function BookPicker() {
+  return (
+    <>
+      <button onClick={() => book = books[Math.floor(Math.random() * 5)]}>Click Me To Pick A Book!</button>
+      <h1>{book.title}</h1>
+    </>
+  );
+}
+
+function DisplayInfo() {
+  return (
+    <>
+      <h3>Author: {book.author}</h3>
+      <p>Genre: {book.genre}</p>
+      <p>ISBN: {book.isbn}</p>
+    </>
+  );
+}
+
+function DisplayInfoButton() {
+  return (
+    <>
+      <button onClick={DisplayInfo}>Display Book Information</button>
+    </>
+  );
+}
+
+function FilmAdapt() {
+  return (
+    <p style = {{color: book.FilmAdaptation ? 'green' : 'red'}}>Has a film adaptation: {book.FilmAdaptation}</p>
+  );
+}
+
+function FilmAdaptButton() {
+  return (
+    <button onClick={FilmAdapt}>Is there a film adaptation?</button>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <ListBooks />
+      <BookPicker />
+      <DisplayInfoButton />
+      <FilmAdaptButton />
+      
     </>
   )
 }
